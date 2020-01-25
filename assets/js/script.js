aa3dc5394cd5f6c51bc19e07ac456a5c8eeab45c
Constanza Avilés Zavando <constanzaavileszavando@MacBook-Pro-de-Constanza.local>
2020-01-25 12:49:25 -0300

    2do commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,30 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 850) {
-	    $("nav").css("background-color", "rgba(231,191,222,1)");}
-	  else{
-		  $("nav").css("background-color", "rgba(231,191,222,0)");}
-  });
+    var scroll=$(window).scrollTop();
+    var winHeight=$(window).height();
+    if (scroll > winHeight-130) {
+      $("nav").css("background-color", "rgba(231,191,222,1)");}
+      else{
+        $("nav").css("background-color", "rgba(231,191,222,0)");}
+      });
 
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  });
+      $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      });
 
-  $(function(){
-    $("a").click(function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var gato = this.hash;
+      $(function(){
+        $("a").click(function(event) {
+          if (this.hash !== "") {
+            event.preventDefault();
+            var gato = this.hash;
 
-          $('html, body').animate({
-            scrollTop: (($(gato).offset().top)-130)
-          }, 900, function(){
-                window.location.hash = gato-130;
-              });
-            }
-          });
+            $('html, body').animate({
+              scrollTop: (($(gato).offset().top)-130)
+            }, 900, function(){
+              window.location.hash = gato-130;
+            });
+          }
         });
+      });
 
-})
+    })
